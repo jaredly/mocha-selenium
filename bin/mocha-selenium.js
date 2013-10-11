cddfbaaaef3b3aa2ce5c730ec00c0f5edc43ff6f
Peter Braden <peterbraden@peterbraden.co.uk>
2013-10-11 14:32:15 -0700

    Take a path to test files. Can pass a test directory as well.

--- bin/mocha-selenium.js
+++ bin/mocha-selenium.js
@@ -5,10 +5,18 @@ var program = require('commander')
 
 program
   .version('0.0.1')
+  .usage('[options] <test files / directories...>')
   .option('-e, --environment [env]', 'Pick the environment to run (host + browsers). Default: local')
   .option('-p, --parallel', 'Run the tests in parallel. Default: false')
   .option('-c, --config [file]', 'Specify the config file. Default: ./selenium.json')
   .parse(process.argv);
+
+if (!program.args.length > 0){
+  program.args = ['.']
+}
+
+program.paths = program.args
+
 
 new Runner(program).execute(function (failed, passed) {
   process.exit(failed ? 1 : 0);
